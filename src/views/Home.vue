60c4b17da1dab0ec81e8f0f8f5756ed5ce284b6c
NikitaSuhotin <nox.dregon@yandex.ru>
2021-10-10 17:44:43 +0300

    поченили соединенгие

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,9 +17,9 @@
           </thead>
           <tbody>
              <tr v-for="(note, index) in notes" :key="index">
-              <td>{{note.Name}}</td>
-              <td>{{note.Body}} </td>
-              <td>{{note.CreateTime}}</td>
+              <td>{{note.name}}</td>
+              <td>{{note.body}} </td>
+              <td>{{note.createTime}}</td>
               <td>
                 <button type="button" class="btn btn-warning btn-sm">Изменить</button>
                 <button type="button" class="btn btn-danger btn-sm">Удалить</button>
@@ -55,7 +55,7 @@ export default {
     },
   },
     created() {
-    this.getBooks();
+    this.getNotes();
   },
 };
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+    <div class="col-lg-5 offset-lg-1 mb-5 mb-lg-0" data-aos="fade-right">
+            <div class="bg-white p-5 shadow">
+            <div class="heading-section text-center">
+            <h2 class="mb-4">
+            Войти
+            </h2>
+            </div>
+            <form method="post" name="contact-us" action="">
+            <div class="row">
+            <div class="col-md-12 form-group">
+            <input type="text" class="form-control" id="name" name="name" placeholder="Логин">
+            </div>
+            <div class="col-md-12 form-group">
+            <input type="text" class="form-control" id="email" name="email" placeholder="Почта">
+            </div>
+            <div class="col-md-12 form-group">
+            <input type="text" class="form-control" id="pas" name="pas" placeholder="Пороль">
+            </div>
+            <div class="col-md-12 text-center">
+            <button class="btn btn-primary btn-shadow btn-lg" type="submit" name="submit">Войти</button>
+            </div>
+            </div>
+            </form>
+            </div>
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
